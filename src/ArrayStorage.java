18b91669fad513a7e640d1ceb289929487558218
genapox1980 <genapox1980@yandex.ru>
2021-06-23 20:39:48 +0400

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[10_000];
+    int size = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                resume = storage[i];
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                size--;
+                storage[i] = storage[size];
+                storage[size] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
